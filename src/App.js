ca409d74a0596da2c620aa13122ce41c050c3139
zCoder <cycs.ingenieria@gmail.com>
2022-04-24 17:36:50 -0400

    Titulo CYCS & Co.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h2>
+          CYCS & Co. | <small>zCoder</small>
+        </h2>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
